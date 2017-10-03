0132fb7998b1f48374221427b302d9a63e1c9726
Timson <timsontse.scem@gmail.com>
2017-10-03 02:15:54 -0700

    Changed it to put the Spanish name as well

--- cards.cpp
+++ cards.cpp
@@ -204,7 +204,7 @@ double Hand::calcValue() const {
 void Hand::printHand() {
 	using namespace std;
 	for (int i = 0; i < v.size(); i++) {
-		cout << v[i].get_english_rank() << " of " << v[i].get_english_suit() << endl;
+		cout << v[i].get_spanish_rank() << " de " << v[i].get_spanish_suit() << " (" << v[i].get_english_rank() << " of " << v[i].get_english_suit() << ")" << endl;
 	}
 	return;
 }
